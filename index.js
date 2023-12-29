1163c951068e8e9f1ab32239e0750d99ef7622eb
Candy <candy.ongaro@student.moringaschool.com>
2023-12-29 11:57:05 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers= ["1", "2", "3", "4", "5"];
+
+const returnFirstTwoDrivers =  function(drivers) {
+    return drivers.slice(0, 2);
+  }
+
+  const returnLastTwoDrivers =  function(drivers) {
+    return drivers.slice(-2);
+  }
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(number){
+
+ return (function(fare){
+   return fare * number
+ })
+  }
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, choiceOfDrivers){
+  return choiceOfDrivers(drivers);
+}
